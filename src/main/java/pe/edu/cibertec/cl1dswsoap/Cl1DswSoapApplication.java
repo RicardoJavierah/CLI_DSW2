04cfa61d047debdcc28cad6bc488b57a816ec4ab
ricardojavierah <ricardojavierah@gmail.com>
2023-09-30 15:09:17 -0500

    Estructura_Base-Ricardo Alvarado

---
+++ src/main/java/pe/edu/cibertec/cl1dswsoap/Cl1DswSoapApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1dswsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1DswSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1DswSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/cl1dswsoap/Cl1DswSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1dswsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1DswSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
